fc958b3a8755092d4cea6d518872612eab257400
FiqhroDeden <dedhensupatmo@gmail.com>
2022-08-02 16:11:13 +0900

    Integration Landing Page, Checkout, My Dashboard

--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Checkout extends Model
 {
     use HasFactory, SoftDeletes;
-    protected $fillable = ['user_id', 'camp_id', 'card_number', 'expaired', 'cvc', 'is_paid'];
+    protected $fillable = ['user_id', 'camp_id', 'card_number', 'expired', 'cvc', 'is_paid'];
+
+    public function setExpiredAttribute($value)
+    {
+        $this->attributes['expired'] = date('Y-m-d', strtotime($value));
+    }
+
+    public function Camp(): BelongsTo
+    {
+        return $this->belongsTo(Camp::class);
+    }
+
 }
